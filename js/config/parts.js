61b3fe862c59b0ea195bc63870cc6781fc9c8dba
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-02 12:59:49 +0200

    feat: config/parts.js, data of drone parts

---
+++ js/config/parts.js
@@ -0,0 +1,48 @@
+const droneParts = {
+    frames: [
+        { name: "Mark 4 7\"", price: 12, compatibility: "7\"" },
+        { name: "Mark 4 v2 10\"", price: 20, compatibility: "10\"" }
+    ],
+    motors: [
+        {
+            name: "FlashHobby 2807 1300kv + Props HQProp 7x4x3",
+            price: 55,
+            compatibility: "7\""
+        },
+        {
+            name: "EMAX 2807 1300kv + Props HQProp 7x4x3",
+            price: 45,
+            compatibility: "7\""
+        },
+        {
+            name: "ReadyToSky 3115 900kv + Props HQ MacroQuad Prop 10x5x3",
+            price: 70,
+            compatibility: "10\""
+        },
+        {
+            name: "BrotherHobby Tornado 3115 900kv + Props HQ MacroQuad Prop 10x5x3",
+            price: 110,
+            compatibility: "10\""
+        }
+    ],
+    batteries: [
+        { name: "6s2p 8000mAh", price: 60, compatibility: "7\" and 10\"" },
+        { name: "6s3p 12000mAh", price: 90, compatibility: "7\" and 10\"" }
+    ],
+    flightControllers: [
+        { name: "SpeedyBee V4 55A", price: 50, compatibility: "7\" and 10\"" },
+        { name: "Mamba F405 MK2", price: 70, compatibility: "7\" and 10\"" }
+    ],
+    cameras: [
+        { name: "Caddx Ratel Pro", price: 30, compatibility: "7\" and 10\"" },
+        { name: "Foxeer Night Cat 3", price: 40, compatibility: "7\" and 10\"" }
+    ],
+    videoAntennas: [
+        { name: "Rush Cherry 2", price: 10, compatibility: "7\" and 10\"" },
+        { name: "SkyZone MushRoom", price: 8, compatibility: "7\" and 10\"" }
+    ],
+    radioModules: [
+        { name: "Bayck ELRS 915mhz", price: 10, compatibility: "7\" and 10\"" },
+        { name: "HappyModel RX 915mhz", price: 15, compatibility: "7\" and 10\"" }
+    ]
+};
