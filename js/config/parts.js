f612269651626f687831c0864cf46da101ab5307
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-11-02 14:07:54 +0200

    feat: add frames, cause they must be there

--- js/config/parts.js
+++ js/config/parts.js
@@ -1,6 +1,10 @@
 import { Images } from "../../common/images/images.js";
 
 const DroneParts = {
+    frames: [
+        { name: "Mark 4 7\"", price: 12, compatibility: "7\"", image: Images.Frame1 },
+        { name: "Mark 4 v2 10\"", price: 20, compatibility: "10\"", image: Images.Frame2}
+    ],
     motors: [
         {
             name: "FlashHobby 2807 1300kv + Props HQProp 7x4x3",
